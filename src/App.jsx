e7e7e5b18a5772cfa9df2f3509631b8b1206e298
Wilbert Chen <wilbertchen2001@gmail.com>
2022-06-18 19:13:10 +0700

    feat: create page wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import Sidebar from './components/Siderbar/Sidebar';
+import Wrapper from './pages/Wrapper/Wrapper';
 
 function App() {
   return (
     <div className='App'>
       <Sidebar />
+      <Wrapper />
     </div>
   );
 }
---
+++ src/pages/Wrapper/Wrapper.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import Profile from '../../assets/profile.png';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
+import './Wrapper.scss';
+
+const Wrapper = () => {
+  const [value, setValue] = useState('');
+  const [show, setShow] = useState(false);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const showInput = () => {
+    setShow(!show);
+  };
+
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      console.log('search');
+    }
+  };
+
+  return (
+    <div className='wrapper'>
+      <div className='nav'>
+        <div className='left-nav'>
+          <span className='path'>Perusahaan</span>
+          <span className='arrow'>&gt;</span>
+          <span className='last-path'>Mitramas Infosys Global</span>
+        </div>
+        <div className='right-nav'>
+          <div className='util'>
+            <div className='search-bar'>
+              <input
+                type='text'
+                value={value}
+                placeholder='Search'
+                onChange={handleChange}
+                onKeyDown={search}
+                style={{ visibility: show ? 'visible' : 'hidden' }}
+              />
+              <SearchRoundedIcon id='search' onClick={showInput} />
+            </div>
+            <NotificationsRoundedIcon />
+          </div>
+          <div className='profile'>
+            <img src={Profile} alt='profile' /> John Doe
+          </div>
+        </div>
+      </div>
+      <div className='content'></div>
+    </div>
+  );
+};
+
+export default Wrapper;
