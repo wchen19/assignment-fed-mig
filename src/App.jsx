a27b9f288330dfc22c056f2f052961faba1e01f6
Wilbert Chen <wilbertchen2001@gmail.com>
2022-06-18 17:26:43 +0700

    feat: create sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css';
+import './App.scss';
+import Sidebar from './components/Siderbar/Sidebar';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Siderbar/Sidebar.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ConfirmationNumberRoundedIcon from '@mui/icons-material/ConfirmationNumberRounded';
+import AssignmentTurnedInRoundedIcon from '@mui/icons-material/AssignmentTurnedInRounded';
+import Inventory2RoundedIcon from '@mui/icons-material/Inventory2Rounded';
+import ApartmentRoundedIcon from '@mui/icons-material/ApartmentRounded';
+import PeopleOutlineRoundedIcon from '@mui/icons-material/PeopleOutlineRounded';
+import LayersRoundedIcon from '@mui/icons-material/LayersRounded';
+import KeyboardCommandKeyRoundedIcon from '@mui/icons-material/KeyboardCommandKeyRounded';
+import ViewInArRoundedIcon from '@mui/icons-material/ViewInArRounded';
+import SavedSearchRoundedIcon from '@mui/icons-material/SavedSearchRounded';
+import './Sidebar.scss';
+
+const Sidebar = () => {
+  const handleClick = (e) => {
+    const active = document.getElementsByClassName('active-tab');
+    if (active.length !== 0) active[0].classList.remove('active-tab');
+    let target = e.target.closest('div.menu');
+    target.classList.add('active-tab');
+  };
+
+  return (
+    <div className='sidebar'>
+      <div className='logo'>logo</div>
+      <div className='menu-container'>
+        <div className='menu' onClick={handleClick}>
+          <HomeRoundedIcon />
+        </div>
+        <div className='menu' onClick={handleClick}>
+          <ConfirmationNumberRoundedIcon />
+        </div>
+        <div className='menu' onClick={handleClick}>
+          <AssignmentTurnedInRoundedIcon />
+        </div>
+        <div className='menu' onClick={handleClick}>
+          <Inventory2RoundedIcon />
+        </div>
+        <div className='menu active-tab' onClick={handleClick}>
+          <ApartmentRoundedIcon />
+        </div>
+      </div>
+      <div className='menu-container'>
+        <div className='menu' onClick={handleClick}>
+          <PeopleOutlineRoundedIcon />
+        </div>
+        <div className='menu' onClick={handleClick}>
+          <LayersRoundedIcon />
+        </div>
+        <div className='menu' onClick={handleClick}>
+          <KeyboardCommandKeyRoundedIcon />
+        </div>
+        <div className='menu' onClick={handleClick}>
+          <ViewInArRoundedIcon />
+        </div>
+        <div className='menu' onClick={handleClick}>
+          <SavedSearchRoundedIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
