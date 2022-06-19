5612fa22c138769d5f6483d1b344a4d73774a6e8
Wilbert Chen <wilbertchen2001@gmail.com>
2022-06-19 16:54:41 +0700

    feat: create Location component

---
+++ src/components/Location/Location.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Card from '../../components/Card/Card';
+import MapsHomeWorkRoundedIcon from '@mui/icons-material/MapsHomeWorkRounded';
+import BuildRoundedIcon from '@mui/icons-material/BuildRounded';
+import WarehouseRoundedIcon from '@mui/icons-material/WarehouseRounded';
+import './Location.scss';
+
+const Location = () => {
+  return (
+    <Card>
+      <div className='location'>
+        <div className='top'>
+          <div className='desc'>Lokasi</div>
+          <span>Lihat semua</span>
+        </div>
+        <div className='bottom'>
+          <div className='box-1'>
+            <MapsHomeWorkRoundedIcon />
+            <div className='detail'>
+              <div className='number'>20</div>
+              <span>Induk</span>
+            </div>
+          </div>
+          <div className='box-2'>
+            <BuildRoundedIcon />
+            <div className='detail'>
+              <div className='number'>3</div>
+              <span>Sub Lokasi Level 1</span>
+            </div>
+          </div>
+          <div className='box-3'>
+            <WarehouseRoundedIcon />
+            <div className='detail'>
+              <div className='number'>1</div>
+              <span>Sub Lokasi Level 2</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default Location;
--- src/pages/Company/Company.jsx
+++ src/pages/Company/Company.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import CompanyInfo from '../../components/CompanyInfo/CompanyInfo';
+import Location from '../../components/Location/Location';
 import './Company.scss';
 
 const Company = () => {
   return (
-    <div className='company'>
+    <div className='company row'>
       <div className='flex-1'>
         <CompanyInfo />
       </div>
-      <div className='flex-3'></div>
+      <div className='flex-3 column'>
+        <div className='flex-1'>
+          <Location />
+        </div>
+        <div className='flex-2'>
+          <Location />
+        </div>
+      </div>
     </div>
   );
 };
